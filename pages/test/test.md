b5b7f9872227e9fbfc8c60816a63a03c74ff32ab
Richard Torruellas <rtorruellas@gmail.com>
2012-12-01 18:27:55 -0700

    adding some serious chicken

---
+++ pages/test/test.md
@@ -0,0 +1,7 @@
+title: Test?
+extends: main_layout.html
+active_page: test/index
+
+###Hmmmm...###
+
+Ill put some tests and examples here later!
---
+++ static_site.py
@@ -0,0 +1,37 @@
+__author__ = 'rtorruellas'
+
+import sys
+import navigation
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
+
+nav = navigation.main_nav_method()
+
+@app.route('/')
+def index():
+    return render_template('index.html', nav=nav)
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page, nav=nav)
+
+@app.route('/sitemap/')
+def site_map():
+    return render_template('map.html', pages=pages, nav=nav)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
